a9251da73444d2e6a7042b79a913d87d5c26362f
Kamal1701 <kk17kannan@yahoo.com>
2023-12-26 10:17:18 +0530

    updated

---
+++ lib/global_variable.dart
@@ -0,0 +1,45 @@
+final products = [
+  {
+    'id': 0,
+    'title': 'Men\'s Nike Shoes',
+    'price': 44.50,
+    'sizes': [8, 9, 10, 11],
+    'company': 'Nike',
+    'imageUrl': 'assets/images/shoes_1.png',
+  },
+  {
+    'id': 1,
+    'title': 'Addidas Shoes',
+    'price': 22.50,
+    'sizes': [9, 10, 11],
+    'company': 'Addidas',
+    'imageUrl': 'assets/images/shoes_2.png',
+  },
+  {
+    'id': 2,
+    'title': 'Bata Women\'s Shoes',
+    'price': 30.50,
+    'sizes': [8, 9, 12],
+    'company': 'Bata',
+    'imageUrl': 'assets/images/shoes_3.png',
+  },
+  {
+    'id': 3,
+    'title': 'Jordan Shoes',
+    'price': 26.50,
+    'sizes': [8, 9, 10, 11],
+    'company': 'Jordan',
+    'imageUrl': 'assets/images/shoes_4.png',
+  },
+];
+
+final cart = [
+  {
+    'id': 0,
+    'title': 'Men\'s Nike Shoes',
+    'price': 44.50,
+    'sizes': [8, 9, 10, 11],
+    'company': 'Nike',
+    'imageUrl': 'assets/images/shoes_1.png',
+  },
+];
